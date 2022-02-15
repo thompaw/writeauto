36bcbc4c7faabc7a6118247745a1df48c44d2d4b
thompaw <98345171+thompaw@users.noreply.github.com>
2022-02-14 21:45:27 -0500

    updated link

--- grab.py
+++ grab.py
@@ -17,7 +17,9 @@ def reqlink(url):
     soup = BeautifulSoup(r.content, 'html.parser')
 
     # Dive in and find main div tag, where everything else I need will branch off of
-    main_div = soup
-    print(main_div)
+    jsdiv = soup.body.find_all('div')[1].find('div', "sc-gsDKAQ cFoFbs")
+    print(jsdiv)    # I am having problems finding the next div tag due to js, this article explains how to bypass 
+    # https://stackoverflow.com/questions/8049520/web-scraping-javascript-page-with-python
 
-reqlink(input('Link: '))
+# reqlink(input('Link: '))
+reqlink('https://hackerone.com/reports/1439026')
