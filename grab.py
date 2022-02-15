630e3777433bca6c61b1d52f09322a91e3a3ecb9
thompaw <98345171+thompaw@users.noreply.github.com>
2022-02-15 13:53:44 -0500

    fresh start

--- grab.py
+++
@@ -1,8 +0,0 @@
-from requests_html import HTMLSession
-from bs4 import BeautifulSoup
-
-
-def reqlink(url):
-    print('dum')
-# reqlink(input('Link: '))
-reqlink('https://hackerone.com/reports/1439026')
