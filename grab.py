02d3fbf20dab8fefaac8250e88ac40f0e02de655
thompaw <98345171+thompaw@users.noreply.github.com>
2022-02-15 21:26:54 -0500

    webdriver headless now

--- grab.py
+++ grab.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
-driver = webdriver.Firefox()
+from selenium.webdriver.firefox.options import Options
+
+firefox_options = Options()
+firefox_options.add_argument("--headless")
+firefox_options.add_argument("--disable-extensions")
+
+driver = webdriver.Firefox(options=firefox_options)
 driver.get('https://avi.im/stuff/js-or-no-js.html')
-p_element = driver.find_element(id_='intro-text')
+p_element = driver.find_element_by_id(id_='intro-text')
 print(p_element.text)
