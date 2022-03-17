e4e2594f69ea24e2244ace173e568746ce58ebf1
thompaw <98345171+thompaw@users.noreply.github.com>
2022-03-17 10:29:34 -0400

    updated grab, added geckodriver for selenium

--- grab.py
+++ grab.py
@@ -4,8 +4,13 @@ from selenium.webdriver.firefox.options import Options
 firefox_options = Options()
 firefox_options.add_argument("--headless")
 firefox_options.add_argument("--disable-extensions")
+driver = webdriver.Firefox(executable_path = "./geckodriver.exe", options=firefox_options)
 
-driver = webdriver.Firefox(options=firefox_options)
-driver.get('https://avi.im/stuff/js-or-no-js.html')
-p_element = driver.find_element_by_id(id_='intro-text')
-print(p_element.text)
+def fetch_page(url): 
+    driver.get(url)
+    maindiv = driver.find_element_by_class_name('content-wrapper da-scroll-area card-drop-shadow')
+    return(maindiv)
+
+# test code below
+print(fetch_page('https://hackerone.com/reports/1309435'))
+# resource: https://medium.com/the-andela-way/introduction-to-web-scraping-using-selenium-7ec377a8cf72 
